464c31e661543be25bcc29999e8001b93320d59a
peggylicious <p.asq@live.com>
2022-03-03 15:57:17 +0100

    Add controller to get all doctors by role'git st

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -8,10 +8,14 @@ const { body, validationResult } = require("express-validator");
 let AuthUser = require("../models/auth");
 
 module.exports.getAllDoctors = (req, res, next) => {
-    AuthUser.doctors.find({role: "doctor"}).select("_id email role").then((doctors) => {
-        console.log(doctors)
-        res.status(200).json({doctors})
-    }).catch(err => {
-        res.status(401).json({err})
+  AuthUser.doctors
+    .find({ role: req.body.role })
+    .select("_id email role")
+    .then((doctors) => {
+      console.log(doctors);
+      res.status(200).json({ doctors });
     })
-}
+    .catch((err) => {
+      res.status(401).json({ err });
+    });
+};
