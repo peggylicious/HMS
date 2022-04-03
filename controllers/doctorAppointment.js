144a4a53c99361b3329f97c5a2f00b5791719742
peggylicious <p.asq@live.com>
2022-04-03 12:07:21 +0100

    Hide unwanted code

--- controllers/doctorAppointment.js
+++ controllers/doctorAppointment.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-var jwt = require("jsonwebtoken");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+// var jwt = require("jsonwebtoken");
 
-const { body, validationResult } = require("express-validator");
+// const { body, validationResult } = require("express-validator");
 
-let DoctorAppointment = require("../models/doctorAppointments");
+// let DoctorAppointment = require("../models/doctorAppointments");
 
-module.exports.createDoctorAppointment = (req, res, next) => {
-    let appointment = new DoctorAppointment({
-        id: mongoose.Types.ObjectId,
-        date: req.body.date,
-        time: req.body.time,
-        doctor_id: req.body.id
-    })
-    appointment.save().then(appointment => {
-        res.status(200).json(appointment)
-    }).catch(err => {
-        res.status(500).json(err)
-    })
-}
+// module.exports.createDoctorAppointment = (req, res, next) => {
+//     let appointment = new DoctorAppointment({
+//         id: mongoose.Types.ObjectId,
+//         date: req.body.date,
+//         time: req.body.time,
+//         doctor_id: req.body.id
+//     })
+//     appointment.save().then(appointment => {
+//         res.status(200).json(appointment)
+//     }).catch(err => {
+//         res.status(500).json(err)
+//     })
+// }
 
 // module.exports.getAppointment = (req, res, next) => {
 //     DoctorAppointment.find({doctor_id: req.body.doctor_id})
